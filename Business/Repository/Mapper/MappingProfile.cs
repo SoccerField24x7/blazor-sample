63889234a899a2e74fb58496a680075cf54432e3
Jesse Quijano <jesse@quijano.net>
2021-09-07 16:59:45 -0700

    automapper mapping profile

---
+++ Business/Repository/Mapper/MappingProfile.cs
@@ -0,0 +1,14 @@
+using AutoMapper;
+using DataAccess.Data;
+using Models;
+
+namespace Business.Repository.Mapper
+{
+    public class MappingProfile : Profile
+    {
+        public MappingProfile()
+        {
+            CreateMap<HotelRoomDTO, HotelRoom>().ReverseMap();
+        }
+    }
+}
